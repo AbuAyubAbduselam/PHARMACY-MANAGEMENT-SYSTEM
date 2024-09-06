5c1b40d203af1a22ef1460d5ba6b2279d3c3d4f0
abuayubabduselam <abuayubabduselam@gmail.com>
2024-09-06 07:09:43 +0300

    7

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Ensure this is set correctly for the deployed environment
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5100/api",
+        target: "https://pharmacy-management-4omz.vercel.app/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
+  },
+  build: {
+    outDir: "dist", // Ensure the output directory is correctly set
   },
 });
